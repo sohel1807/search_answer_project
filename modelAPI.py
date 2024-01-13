cff640c78c4d674d291a30ba01605c765c944112
sahil <sahilmulani501@gmail.com>
2024-01-14 00:40:22 +0530

    test : api deployment test 3

--- modelAPI.py
+++ modelAPI.py
@@ -2,6 +2,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from project import startModel  # Correct import
+from http.server import BaseHTTPRequestHandler
+
+
+class handler(BaseHTTPRequestHandler):
+ 
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!'.encode('utf-8'))
+        return
 
 class InputModel(BaseModel):
     query: str
