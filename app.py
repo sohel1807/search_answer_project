a3755438afd73df9dc0d3801289f95f6578adf7d
sahil <sahilmulani501@gmail.com>
2024-01-14 11:05:51 +0530

    test : remove http vercel method

--- app.py
+++ app.py
@@ -2,17 +2,9 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from model import startModel  # Correct import
-from http.server import BaseHTTPRequestHandler
 
 
-class handler(BaseHTTPRequestHandler):
- 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        return
+
 
 class InputModel(BaseModel):
     query: str
